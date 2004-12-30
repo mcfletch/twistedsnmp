31a17e2604d381e5858378dffd592de6c7d2de02
Mike C. Fletcher <mcfletch@vrplumber.com>
2004-12-30 10:28:13 +0000

    Add ability to insert sub-oidstores in BisectOIDStore objects, also ability to insert callable objects to return calculated values.

--- bisectoidstore.py
+++ bisectoidstore.py
@@ -29,6 +29,16 @@ class BisectOIDStore(oidstore.OIDStore):
 	where a small OID set is to be loaded and tested against.
 
 	It should be available on any Python installation.
+
+	This is the only storage which allows for chaining
+	storages (i.e. inserting a storage as a child of a
+	storage) and run-time-calculated values (inserting
+	a callable object which calculates values on
+	retrieval).
+
+	Signature for a callable value is:
+		def callableValue( oid, storage ):
+			return finalValue
 	"""
 	def __init__( self, OIDs=None ):
 		"""Initialise the storage with appropriate OIDs"""
@@ -44,13 +54,31 @@ class BisectOIDStore(oidstore.OIDStore):
 		base = oidToSortable( base )
 		start = bisect.bisect( self.OIDs, (base,) )
 		try:
-			return (sortableToOID(base), self.OIDs[start][1])
+			oid,result = self.OIDs[start]
 		except (IndexError,KeyError):
 			# do error reporting here
 			raise errors.OIDNameError(
 				base,
 				message="OID not found in database",
 			)
+		else:
+			if oid != base:
+				try:
+					oid,result = self.OIDs[start-1]
+				except IndexError, err:
+					pass
+				else:
+					if (
+						oidstore.dumbPrefix( oid, base ) and
+						hasattr( result,'getExactOID')
+					):
+						return result.getExactOID( sortableToOID(base) )
+				raise errors.OIDNameError(
+					base,
+					message="OID not found in database",
+				)
+			return sortableToOID(base), self.returnValue(result,base)
+	
 	def setValue( self, oid, value):
 		"""Set the given oid,value pair, returning old value
 
@@ -59,11 +87,20 @@ class BisectOIDStore(oidstore.OIDStore):
 		"""
 		oid = oidToSortable( oid )
 		start = bisect.bisect( self.OIDs, (oid,) )
+		previousTable = (
+			start and self.OIDs and
+			oidstore.dumbPrefix( self.OIDs[start-1][0], oid ) and
+			hasattr(self.OIDs[start-1][1],'setValue')
+		)
 		if start < len(self.OIDs):
 			oldOID, oldValue = self.OIDs[ start ]
 			if oldOID == oid:
 				self.OIDs[start] = (oid,value)
 				return oldValue
+			elif previousTable:
+				return self.OIDs[start-1][1].setValue( sortableToOID(oid), value )
+		elif previousTable:
+			return self.OIDs[start-1][1].setValue( sortableToOID(oid), value )
 		self.OIDs.insert( start, (oid,value))
 		return None
 	def firstOID( self ):
@@ -73,7 +110,21 @@ class BisectOIDStore(oidstore.OIDStore):
 		"""
 		if self.OIDs:
 			oid,value = self.OIDs[0]
-			return sortableToOID( oid ), value
+			if hasattr( value, 'firstOID' ):
+				index = 0
+				while hasattr( value, 'firstOID' ):
+					try:
+						return value.firstOID()
+					except errors.OIDNameError, err:
+						index += 1
+						try:
+							oid,value = self.OIDs[index]
+						except IndexError:
+							raise errors.OIDNameError(
+								(),
+								message="""No OIDs available in this storage""",
+							)
+			return sortableToOID( oid ), self.returnValue(value,oid)
 		else:
 			raise errors.OIDNameError(
 				(),
@@ -99,17 +150,46 @@ class BisectOIDStore(oidstore.OIDStore):
 			elif oid != base and oidstore.dumbPrefix( base, oid ):
 				# if the found OID is prefixed by key, we want to return this OID
 				# otherwise we want to return the *next* item
-				pass
+				
+				# iff the value is a sub-storage, we need to get it to
+				# see if it's got the proper value within it, if not, then
+				# try the next oidstore/value
+				if hasattr( value, 'nextOID' ):
+					try:
+						return value.nextOID( sortableToOID(base) )
+					except errors.OIDNameError, err:
+						start += 1
 			else:
 				# otherwise return the item *after* the found OID (exact match)
+				# again, if the value is a sub-storage, then we need to search
+				# forward from the item after...
 				start += 1
 			# now get the real value...
-			if start < len(self.OIDs ):
+			while start < len(self.OIDs ):
 				key,value = self.OIDs[start]
-				return sortableToOID(key),value
-			else:
-				# overflow error, reached end of our OID table with this OID
-				raise errors.OIDNameError( base, message="""OID is beyond end of table""" )
+				if hasattr( value, 'firstOID' ):
+					try:
+						return value.firstOID()
+					except errors.OIDNameError, err:
+						pass
+				else:
+					return sortableToOID(key),self.returnValue(value,key)
+				start += 1
+			# overflow error, reached end of our OID table with this OID
+			raise errors.OIDNameError( base, message="""OID is beyond end of table""" )
 		else:
 			# starting OID is beyond end of table
+			# only chance is if the last element is a tabular record
+			if self.OIDs:
+				if (
+					hasattr(self.OIDs[-1][1],'nextOID') and
+					oidstore.dumbPrefix( base, self.OIDs[-1][0])
+				):
+					return self.OIDs[-1][1].nextOID( sortableToOID(base) )
 			raise errors.OIDNameError( base, message="""OID is beyond end of table""" )
+
+	def returnValue( self, value, oid ):
+		"""Return value, or value.calculateOIDValue( oid, self )"""
+		if callable( value ):
+			return value( sortableToOID(oid), self )
+		return value
--- test/test_storage.py
+++ test/test_storage.py
@@ -1,4 +1,4 @@
-from twistedsnmp import bisectoidstore, agent
+from twistedsnmp import bisectoidstore, agent, errors
 import unittest
 from twistedsnmp.pysnmpproto import v2c,v1, error
 try:
@@ -90,7 +90,161 @@ class StorageTest( unittest.TestCase ):
 		)
 		r = list(iter(store))
 		assert len(r) == 7
-		
+
+class RecursiveTest( unittest.TestCase ):
+	"""Test Bisect OIDStore with nested storages"""
+	def testExact( self ):
+		"""Do we get child OIDstore's exact values?"""
+		store = bisectoidstore.BisectOIDStore(
+			[
+				('.1.3.6.1.2.1.1.1.0', 'Hello world!'),
+				('.1.3.6.1.2.1.1.2.0', 32),
+				('.1.3.6.1.2.1.1.4.0', v1.OctetString('From Octet String')),
+			]
+		)
+		store.setValue(
+			'.1.3.6.1.2.1.1.3',
+			bisectoidstore.BisectOIDStore(
+				[
+					('.1.3.6.1.2.1.1.3.%d'%i,i)
+					for i in range(32)
+				]
+			)
+		)
+		result = store.getExactOID( '.1.3.6.1.2.1.1.3.5' )
+		assert result[0] == '.1.3.6.1.2.1.1.3.5', result
+		assert result[1] == 5, result
+	def testFirst( self ):
+		"""Test that firstOID properly chains in/out of sub-storages"""
+		store = bisectoidstore.BisectOIDStore(
+			[
+			]
+		)
+		store.setValue( 
+			'.1.3.6.1.2.1.1.3',
+			bisectoidstore.BisectOIDStore(
+				[
+				]
+			)
+		)
+		self.failUnlessRaises(
+			errors.OIDNameError,
+			store.firstOID,
+		)
+		store.setValue(
+			'.1.3.6.1.2.1.1.4.0',
+			32,
+		)
+		result = store.firstOID( )
+		assert result==('.1.3.6.1.2.1.1.4.0', 32, ), result
+		store.getExactOID( '.1.3.6.1.2.1.1.3')[1].setValue(
+			'.1.3.6.1.2.1.1.3.0',
+			32,
+		)
+		result = store.firstOID( )
+		assert result==('.1.3.6.1.2.1.1.3.0', 32, ), result
+	def testSet( self ):
+		"""Test that setting a value chains into sub-storages"""
+		store = bisectoidstore.BisectOIDStore(
+			[
+			]
+		)
+		store.setValue( 
+			'.1.3.6.1.2.1.1.3',
+			bisectoidstore.BisectOIDStore(
+				[
+				]
+			)
+		)
+		store.setValue(
+			'.1.3.6.1.2.1.1.3.4',
+			32,
+		)
+		subStorage = store.getExactOID( '.1.3.6.1.2.1.1.3')[1]
+		assert len(subStorage.OIDs) == 1, (subStorage.OIDs,store.OIDs)
+		store.setValue(
+			'.1.3.6.1.2.1.1.4.0',
+			32,
+		)
+		assert len(subStorage.OIDs) == 1, (subStorage.OIDs,store.OIDs)
+		assert len(store.OIDs) == 2, store.OIDs
+		store.setValue(
+			'.1.3.6.1.2.1.1.0.0',
+			32,
+		)
+		assert len(subStorage.OIDs) == 1, (subStorage.OIDs,store.OIDs)
+		assert len(store.OIDs) == 3, store.OIDs
+		store.setValue( 
+			'.1.3.6.1.2.1.1.3.5',
+			bisectoidstore.BisectOIDStore(
+				[
+				]
+			)
+		)
+		assert len(subStorage.OIDs) == 2, (subStorage.OIDs,store.OIDs)
+		store.setValue( 
+			'.1.3.6.1.2.1.1.3.5.3',
+			32,
+		)
+		assert len(subStorage.OIDs) == 2, (subStorage.OIDs,store.OIDs)
+		subStorage2 = store.getExactOID( '.1.3.6.1.2.1.1.3.5')[1]
+		assert len(subStorage2.OIDs) == 1, (subStorage2.OIDs,subStorage.OIDs)
+	def testNext( self ):
+		"""Test that getNext properly chains into/out-of sub-storages"""
+		store = bisectoidstore.BisectOIDStore(
+			[
+			]
+		)
+		self.failUnlessRaises(
+			errors.OIDNameError,
+			store.nextOID,
+			'.1.3.6.1',
+		)
+		store.setValue( 
+			'.1.3.6.1.2.1.1.3',
+			bisectoidstore.BisectOIDStore(
+				[
+				]
+			)
+		)
+		subStorage = store.getExactOID( '.1.3.6.1.2.1.1.3')[1]
+		self.failUnlessRaises(
+			errors.OIDNameError,
+			store.nextOID,
+			'.1.3.6.1',
+		)
+		store.setValue(
+			'.1.3.6.1.2.1.1.3.4',
+			32,
+		)
+		result = store.nextOID( '.1.3.6.1' )
+		assert result == ( '.1.3.6.1.2.1.1.3.4', 32 ), result
+		store.setValue(
+			'.1.3.6.1.2.1.1.3.3',
+			44,
+		)
+		result = store.nextOID( '.1.3.6.1' )
+		assert result == ( '.1.3.6.1.2.1.1.3.3', 44 ), result
+	def testCalculated( self ):
+		store = bisectoidstore.BisectOIDStore(
+			[
+				('.1.3.6.1.2.1.1.1.0', lambda oid,store: 550),
+				('.1.3.6.1.2.12.1.2.0', lambda oid,store: 555 ),
+				('.1.3.6.1.2.2.1.3.0', v1.IpAddress('127.0.0.1')),
+				('.1.3.6.1.2.1.1.4.0', v1.OctetString('From Octet String')),
+			]
+		)
+		result = store.getExactOID( '.1.3.6.1.2.12.1.2.0' )
+		assert result[0] == '.1.3.6.1.2.12.1.2.0', result
+		assert result[1] == 555, result
+		result = store.nextOID( '.1.3.6.1.2.2.1.3.0' )
+		assert result[0] == '.1.3.6.1.2.12.1.2.0', result
+		assert result[1] == 555, result
+		result = store.firstOID(  )
+		assert result[0] == '.1.3.6.1.2.1.1.1.0', result
+		assert result[1] == 550, result
+
+	
 		
 if bsdoidstore:
 	class BSDTest( object ):
